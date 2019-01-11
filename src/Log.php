e40df1dae8f15a16ae777d26c6ca7544d8b160f1
ayflying <ay@7cuu.com>
2019-01-11 18:07:08 +0800

    完善

--- src/Log.php
+++ src/Log.php
@@ -7,9 +7,24 @@
  */
 namespace Ayflying\Log;
 
-// 插件类
+// 日志类
 class Log{
-    private $dir;
+    /**
+     * 数据库配置
+     * @var array
+     */
+    protected  static $config = [
+        //'path' => __DIR__ . '/../../public/log/',
+        'path' => '/opt/data/wwwroot/heibang/public/log/',
+    ];
+    /**
+     * 日志写入驱动
+     * @var string
+     */
+    static $driver = 'File';
+    static $dir;
+    static $time;
+    static $name;
 
 
     /**
@@ -18,7 +33,9 @@ class Log{
      */
     function __construct($info)
     {
-        
+
+
+
 
     }
 
@@ -27,24 +44,55 @@ class Log{
      * 记录日志信息到内存
      * @param string|array $data
      */
-    public static  function record($data){
+    public static function record($data){
+
 
     }
 
     /**
      * 把保存在内存中的日志信息（用指定的记录方式）写入，并清空内存中的日志
+     * @param string $name
      * @param string|array $data
+     * @return bool
      */
-    public static  function save($data){
+    public static function save($name,$data){
+        self::format($data);
+
+
+        $destination = self::$config['path'] . date('Ym') . '/' . date('d') . '/' . $name  . '.log';
+
+        $path = dirname($destination);
+        //echo $path;
+
+        //return $path;
+        !is_dir($path) && mkdir($path, 0755, true);
+        $message = self::format($data);
+        return error_log($message,3,$destination);
 
     }
 
     /**
      * 实时写入一条日志信息，会触发save操作
+     * @param string $name
      * @param string|array $data
+     * @return bool
      */
-    public static  function write($data){
+    public static function write($name,$data){
 
+        return self::save($name,$data);
+
+
+    }
+
+    /**
+     * 转换格式
+     * @param $arr
+     * @return string
+     */
+    private static function format($arr){
+
+        $str = implode("|",$arr);
+        return $str;
     }
 
 
